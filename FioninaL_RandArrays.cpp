a020edda88ba70ac320f6caf3c32b72a3b85d333
LFionina <80356955+LFionina@users.noreply.github.com>
2021-10-19 21:47:29 +0400

    файл задача генерации случайного массива

---
+++ FioninaL_RandArrays.cpp
@@ -0,0 +1,48 @@
+# include <iostream>
+# include "TXLib.h"
+
+void PrintArray (int data[], int size, const char title[], int column);
+void FillRandomArray (int data[], int size, int range);
+
+//-----------------------------------------------------------------------------
+int main ()
+    {
+    int data [20] = {};
+
+    $y; printf("\n\n 1 задача ------------------------------------\n");
+
+    FillRandomArray (data, 20, 10);
+    PrintArray (data, 20, "Массив задание 1 способ 1", 5);
+
+    return 0;
+    }
+
+//-----------------------------------------------------------------------------
+void FillRandomArray (int data[], int size, int range)
+    {
+    for (int i = 0; i < size; i++)
+        {
+        data[i] = rand() % range - range / 2;
+        }
+    }
+
+//-----------------------------------------------------------------------------
+void PrintArray (int data[], int size, const char title[], int column)
+    {
+    $y; printf ("\n%s:", title);
+
+    for (int i = 0; i < size; i++)
+        {
+        if (i % column == 0) printf ("\n");
+
+        $c; printf ("[%2d] = ",i);
+        $g; printf ("%3d",data[i]);
+        if (i != size - 1)
+            {
+            $c; printf (", ");
+            }
+        }
+
+    printf ("\n");
+    }
+
